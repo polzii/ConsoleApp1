9faf577cb4887e62177439fdaa524f2e9640f59c
Asus <Asus@ASUS->
2021-12-09 18:59:10 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿int num1 = 247653;
+int num2 = 857348;
+
+if (num1 > num2)
+{
+    Console.WriteLine($"Число {num1} больше числа {num2}");
+}
+else if (num1 < num2)
+{
+    Console.WriteLine($"Число {num2} больше числа {num1}");
+}
+else
+{
+    Console.WriteLine($"Число { num1} равно числу { num2}");
+}
+Console.ReadKey();
+    
+
+
+
+
